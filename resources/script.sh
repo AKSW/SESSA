7c7f395c1efed3973d4233d49cfd06a0b16a5cb1
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2016-11-08 07:17:45 +0100

    Update script.sh
    Updated to latest version

--- resources/script.sh
+++ resources/script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-wget http://downloads.dbpedia.org/3.9/en/mappingbased_properties_en.ttl.bz2
-wget http://downloads.dbpedia.org/3.9/en/labels_en.ttl.bz2
-bunzip2 mappingbased_properties_en.ttl.bz2
+wget http://downloads.dbpedia.org/2016-04/core-i18n/en/mappingbased_literals_en.ttl.bz2
+wget http://downloads.dbpedia.org/2016-04/core-i18n/en/mappingbased_objects_en.ttl.bz2
+wget http://downloads.dbpedia.org/2016-04/core-i18n/en/labels_en.ttl.bz2
+bunzip2 mappingbased_literals_en.ttl.bz2
+bunzip2 mappingbased_objects_en.ttl.bz2
 bunzip2 labels_en.ttl.bz2 
 
 mv labels_en.ttl dbpedia_labels.ttl
 
 wget http://139.18.2.164/rusbeck/hawk/en_surface_forms.ttl
-wget https://raw.githubusercontent.com/AKSW/hawk/master/resources/dbpedia_3Eng_class.ttl
-wget https://raw.githubusercontent.com/AKSW/hawk/master/resources/dbpedia_3Eng_property.ttl
+wget https://raw.githubusercontent.com/AKSW/NLIWOD/master/qa.hawk/resources/dbpedia_3Eng_class.ttl
+wget https://raw.githubusercontent.com/AKSW/NLIWOD/master/qa.hawk/resources/dbpedia_3Eng_property.ttl
