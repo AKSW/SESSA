89abc7b0c16338b5767f8c71ef3d5928999e9bb0
Simon Bordewisch <sbordewisch@gmx.de>
2017-06-30 14:49:19 +0200

    Add tests for all neighbor-methods

--- src/test/java/org/aksw/sessa/helper/graph/GraphTest.java
+++ src/test/java/org/aksw/sessa/helper/graph/GraphTest.java
@@ -13,14 +13,15 @@ import org.junit.Test;
  */
 public class GraphTest {
 
-  private Graph graph = new Graph();
+  private Graph graph;
   List<Node> nodes;
 
 
   @Before
   public void initialize(){
+    graph = new Graph();
     nodes = new ArrayList<>();
-    for (int i=0;i<4;i++){
+    for (int i=0;i<5;i++){
       Node<Integer> node = new Node<>(i);
       nodes.add(node);
       graph.addNode(node);
@@ -39,7 +40,7 @@ public class GraphTest {
   }
 
   @Test
-  public void testGetNeighbors() {
+  public void testGetNeighborsLeadingFrom_NotEmptyTests() {
     Set<Node> neighborOf1 = new HashSet<>();
     neighborOf1.add(nodes.get(2));
     neighborOf1.add(nodes.get(3));
@@ -50,4 +51,40 @@ public class GraphTest {
     Assert.assertEquals(neighborOf3,graph.getNeighborsLeadingFrom(nodes.get(3)));
   }
 
+  @Test
+  public void testGetNeighborsLeadingFrom_EmptyNeighbours() {
+    Assert.assertEquals(new HashSet<Node>(),graph.getNeighborsLeadingFrom(nodes.get(4)));
+  }
+
+  @Test
+  public void testGetNeighborsLeadingTo_NotEmptyTests() {
+    Set<Node> neighbor = new HashSet<>();
+    neighbor.add(nodes.get(0));
+    neighbor.add(nodes.get(1));
+    Assert.assertEquals(neighbor,graph.getNeighborsLeadingTo(nodes.get(2)));
+
+    Set<Node> leadsTo0 = new HashSet<>();
+    leadsTo0.add(nodes.get(3));
+    Assert.assertEquals(leadsTo0,graph.getNeighborsLeadingTo(nodes.get(0)));
+  }
+
+  @Test
+  public void testGetNeighborsLeadingTo_EmptyNeighbours() {
+    Assert.assertEquals(new HashSet<Node>(),graph.getNeighborsLeadingTo(nodes.get(4)));
+  }
+
+  @Test
+  public void testGetAllNeighbors_NotEmptyTests() {
+    Set<Node> allNeighborsOf1 = new HashSet<>();
+    allNeighborsOf1.add(nodes.get(0));
+    allNeighborsOf1.add(nodes.get(2));
+    allNeighborsOf1.add(nodes.get(3));
+
+    Assert.assertEquals(allNeighborsOf1,graph.getAllNeighbors(nodes.get(1)));
+  }
+
+  @Test
+  public void testGetAllNeighbors_EmptyNeighbours() {
+    Assert.assertEquals(new HashSet<Node>(),graph.getNeighborsLeadingTo(nodes.get(4)));
+  }
 }
