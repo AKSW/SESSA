990c1384c5b3235e2e9028e9d5a24e5d8d431e30
Simon Bordewisch <sbordewisch@gmx.de>
2017-07-21 12:43:21 +0200

    Combine all components of this project into one main class

--- src/main/java/org/aksw/sessa/main/SESSA.java
+++ src/main/java/org/aksw/sessa/main/SESSA.java
@@ -1,20 +1,44 @@
 package org.aksw.sessa.main;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+import org.aksw.sessa.candidate.CandidateGenerator;
+import org.aksw.sessa.colorspreading.ColorSpreader;
+import org.aksw.sessa.helper.graph.Node;
+import org.aksw.sessa.importing.dictionary.DictionaryImportInterface;
+import org.aksw.sessa.importing.dictionary.implementation.TsvDictionaryImport;
+import org.aksw.sessa.query.models.NGramEntryPosition;
+import org.aksw.sessa.query.models.NGramHierarchy;
+
 /**
  * Main class of project SESSA, which returns answers to asked questions.
  */
 public class SESSA {
+  
+  private Map<String, Set<String>> dictionary;
 
-  public SESSA() {
-
+  public SESSA(String fileName) {
+    DictionaryImportInterface dictImporter = new TsvDictionaryImport();
+    dictionary = dictImporter.getDictionary(fileName);
   }
 
-  public String answer(String question){
+  public Set<String> answer(String question){
     if (question.equals("")) {
-      return "";
-    } else {
-
       return null;
+    } else {
+      NGramHierarchy nGramHierarchy = new NGramHierarchy(question);
+      CandidateGenerator canGen = new CandidateGenerator(dictionary);
+      Map<NGramEntryPosition,Set<String>> canMap = canGen.getCandidateMapping(nGramHierarchy);
+      ColorSpreader colorSpreader = new ColorSpreader(canMap);
+      colorSpreader.spreadColors();
+      Set<Node> results = colorSpreader.getResult();
+      System.out.println(colorSpreader.getGraph());
+      Set<String> stringResults = new HashSet<>();
+      for(Node result : results){
+        stringResults.add(result.getContent().toString());
+      }
+      return stringResults;
     }
   }
 }
