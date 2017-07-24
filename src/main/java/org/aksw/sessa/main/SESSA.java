b328e63c3a08f6341ca00c2ec36f7e68ef4cae50
Simon Bordewisch <sbordewisch@gmx.de>
2017-07-24 15:34:30 +0200

    Add javadoc

--- src/main/java/org/aksw/sessa/main/SESSA.java
+++ src/main/java/org/aksw/sessa/main/SESSA.java
@@ -18,11 +18,24 @@ public class SESSA {
 
   private Map<String, Set<String>> dictionary;
 
+  /**
+   * Initilize class with a tsv file which contains a dictionary
+   * with URIs as key and keywords to the URI as values.
+   * This is needed for the candidate mapping.
+   * @param fileName tsv file which contains mapping.
+   */
   public SESSA(String fileName) {
     DictionaryImportInterface dictImporter = new TsvDictionaryImport();
     dictionary = dictImporter.getDictionary(fileName);
   }
 
+  /**
+   * This method tries to answer the given question using the method descripted
+   * in the <a href="https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxubGl3b2QyMDE0fGd4Ojc5NjU1YjhhMzNhMDczNWI">corresponding paper</a>.
+   * The answer is a set of strings containg the URIs with the highest likelihood to be the answer (i.e. with the highest explanation score).
+   * @param question the question that should be answered (for now keyword based, i.e. 'birthplace bill gates wife' instead of "Where was Bill Gates' wife born")
+   * @return set of URIs with the highest explanation score
+   */
   public Set<String> answer(String question) {
     if (question.equals("")) {
       return null;
@@ -33,7 +46,6 @@ public class SESSA {
       ColorSpreader colorSpreader = new ColorSpreader(canMap);
       colorSpreader.spreadColors();
       Set<Node> results = colorSpreader.getResult();
-      System.out.println(colorSpreader.getGraph());
       Set<String> stringResults = new HashSet<>();
       for (Node result : results) {
         stringResults.add(result.getContent().toString());
