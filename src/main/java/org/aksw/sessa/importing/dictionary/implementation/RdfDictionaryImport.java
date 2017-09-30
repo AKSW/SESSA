cfc5a54fc2132f832397e15e7fc838eb3e6a8bc7
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2017-09-30 23:08:51 +0200

    converted RDF dict to stream based

--- src/main/java/org/aksw/sessa/importing/dictionary/implementation/RdfDictionaryImport.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/implementation/RdfDictionaryImport.java
@@ -1,18 +1,16 @@
 package org.aksw.sessa.importing.dictionary.implementation;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.aksw.sessa.importing.dictionary.DictionaryImportInterface;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.sparql.core.Quad;
 import org.openrdf.model.vocabulary.RDFS;
 
 public class RdfDictionaryImport implements DictionaryImportInterface {
@@ -21,37 +19,58 @@ public class RdfDictionaryImport implements DictionaryImportInterface {
 	public Map<String, Set<String>> getDictionary(String fileName) {
 
 		Map<String, Set<String>> dictionary = new HashMap<>();
-		Model model = ModelFactory.createDefaultModel();
-		try {
-			model.read(new FileInputStream(fileName), null, "NT");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		StmtIterator iter = model.listStatements();
-		while (iter.hasNext()) {
-			Statement statement = iter.nextStatement(); // get next statement
 
-			String uri = statement.getPredicate().asResource().getURI();
-			String rdfslabeluri = RDFS.LABEL.stringValue();
-			if (uri.equals(rdfslabeluri)) {
-				RDFNode object = statement.getObject();
-				String surfaceform = object.asLiteral().getString();
+		StreamRDF destination = new StreamRDF() {
 
-				if (dictionary.containsKey(statement.getSubject().toString())) {
-					// we see the uri a second time, and thus add the
-					// surfaceform to the set
-					Set<String> tmpset = dictionary.get(statement.getSubject().toString());
-					tmpset.add(surfaceform);
-					dictionary.put(statement.getSubject().toString(), tmpset);
-				} else {
-					// we see the (uri,surfaceform) pair for the first time
-					Set<String> surfaceformset = new HashSet<String>();
-					surfaceformset.add(surfaceform);
-					dictionary.put(statement.getSubject().toString(), surfaceformset);
+			@Override
+			public void triple(Triple statement) {
+
+				String uri = statement.getPredicate().getURI();
+				String rdfslabeluri = RDFS.LABEL.stringValue();
+				if (uri.equals(rdfslabeluri)) {
+					Node object = statement.getObject();
+					String surfaceform = object.getLiteral().getLexicalForm();
+
+					if (dictionary.containsKey(statement.getSubject().toString())) {
+						// we see the uri a second time, and thus add the
+						// surfaceform to the set
+						Set<String> tmpset = dictionary.get(statement.getSubject().toString());
+						tmpset.add(surfaceform);
+						dictionary.put(statement.getSubject().toString(), tmpset);
+					} else {
+						// we see the (uri,surfaceform) pair for the first time
+						Set<String> surfaceformset = new HashSet<String>();
+						surfaceformset.add(surfaceform);
+						dictionary.put(statement.getSubject().toString(), surfaceformset);
+					}
 				}
+
 			}
-		}
+
+			@Override
+			public void base(String arg0) {
+			}
+
+			@Override
+			public void finish() {
+			}
+
+			@Override
+			public void prefix(String arg0, String arg1) {
+			}
+
+			@Override
+			public void quad(Quad arg0) {
+			}
+
+			@Override
+			public void start() {
+			}
+
+		};
+		RDFDataMgr.parse(destination, fileName);
+
 		return dictionary;
 	}
+
 }
