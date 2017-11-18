78276107ae3c969393fe611db36105e90e01009b
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-18 15:42:52 +0100

    Change FileBasedDictionary to reflect new handling

--- src/main/java/org/aksw/sessa/importing/dictionary/FileBasedDictionaryImport.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/FileBasedDictionaryImport.java
@@ -3,17 +3,26 @@ package org.aksw.sessa.importing.dictionary;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import org.aksw.sessa.helper.files.handler.FileHandlerInterface;
 
 public abstract class FileBasedDictionaryImport implements DictionaryImportInterface {
 
   protected Map<String, Set<String>> dictionary;
 
-  public void putAll(String fileName){
-    dictionary.putAll(createDictionary(fileName));
+  /**
+   * Adds the entries in the give handler to the dictionary.
+   * @param handler handler with file information
+   */
+  public void putAll(FileHandlerInterface handler){
+    dictionary.putAll(createDictionary(handler));
   }
 
-  protected abstract Map<String, Set<String>> createDictionary(String fileName);
+  protected abstract Map<String, Set<String>> createDictionary(FileHandlerInterface handler);
 
+  /**
+   * Returns a set view of the mappings contained in this map.
+   * @return a set view of the mappings contained in this map
+   */
   public Set<Entry<String, Set<String>>> entrySet() {
     return dictionary.entrySet();
   }
