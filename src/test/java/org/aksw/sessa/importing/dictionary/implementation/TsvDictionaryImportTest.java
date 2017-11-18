00798745a296eb08ed3070887f9df6896052a556
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-18 12:45:25 +0100

    Split test cases and reformat TsvDictionaryImportTest

--- src/test/java/org/aksw/sessa/importing/dictionary/implementation/TsvDictionaryImportTest.java
+++ src/test/java/org/aksw/sessa/importing/dictionary/implementation/TsvDictionaryImportTest.java
@@ -1,9 +1,7 @@
 package org.aksw.sessa.importing.dictionary.implementation;
 
 import java.util.Set;
-import java.util.Map;
 import org.aksw.sessa.importing.dictionary.DictionaryImportInterface;
-import org.aksw.sessa.importing.dictionary.FileBasedDictionaryImport;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,27 +15,28 @@ public class TsvDictionaryImportTest {
   private final String fileName = "src/test/resources/en_surface_forms_small.tsv";
 
   @Before
-  public void init(){
-    if(dictionary == null) {
+  public void init() {
+    if (dictionary == null) {
       dictionary = new TsvDictionaryImport(fileName);
     }
   }
 
-    @Test
-    public void getDictionary_TestBillGates(){
-      String uri = "http://dbpedia.org/resource/Bill_Gates";
-      Set<String> set = dictionary.get("bill gates");
-      System.out.println(set);
-      Assert.assertTrue(set.contains(uri));
+  //TODO: find a way to avoid code duplication (see LuceneDictionaryTest)
+  @Test
+  public void get_TestTwoWords() {
+    String uri = "http://dbpedia.org/resource/Bill_Gates";
+    Set<String> set = dictionary.get("bill gates");
+    Assert.assertTrue(set.contains(uri));
+  }
 
-      uri = "http://dbpedia.org/ontology/birthPlace";
-      set = dictionary.get("birthplace");
-      System.out.println(set);
-      Assert.assertTrue(set.contains(uri));
+  @Test
+  public void get_TestOneWord() {
+    String uri = "http://dbpedia.org/ontology/birthPlace";
+    Set<String> set = dictionary.get("birthplace");
+    Assert.assertTrue(set.contains(uri));
 
-      uri = "http://dbpedia.org/ontology/spouse";
-      set = dictionary.get("wife");
-      System.out.println(set);
-      Assert.assertTrue(set.contains(uri));
+    uri = "http://dbpedia.org/ontology/spouse";
+    set = dictionary.get("wife");
+    Assert.assertTrue(set.contains(uri));
   }
 }
