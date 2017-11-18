a46215ed46211fe500f3c5e0f4e9cabc45afad10
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-18 15:46:48 +0100

    Changed how ReversedTsvDictionarySaver is handled

--- src/main/java/org/aksw/sessa/main/ReversedTsvDictionarySaver.java
+++ src/main/java/org/aksw/sessa/main/ReversedTsvDictionarySaver.java
@@ -1,52 +1,57 @@
 package org.aksw.sessa.main;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map.Entry;
 import java.util.Set;
+import org.aksw.sessa.helper.files.handler.FileHandlerInterface;
+import org.aksw.sessa.helper.files.handler.RdfFileHandler;
 import org.aksw.sessa.importing.dictionary.FileBasedDictionaryImport;
-import org.aksw.sessa.importing.dictionary.implementation.RdfDictionaryImport;
+import org.aksw.sessa.importing.dictionary.implementation.HashMapDictionary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReversedTsvDictionarySaver {
 
-  static String RDF_labels = "src/main/resources/labels_en.ttl";
-  static String RDF_ontology = "src/main/resources/dbpedia_2016-10.nt";
 
-  private static final String target = "src/main/resources/dictionary.tsv";
+  private static final Logger log = LoggerFactory.getLogger(ReversedTsvDictionarySaver.class);
 
   private static FileBasedDictionaryImport dictionary;
 
-  public static void main(String[] args) {
+  public static void saveDictionary(String target, String... sources) {
 
-		loadFileToDictionaryRDF(RDF_labels);
-		loadFileToDictionaryRDF(RDF_ontology);
-    saveDictionary();
-
+    for(String source : sources )
+    {
+      loadFileToHashMap(source);
+    }
+    saveDictionary(target);
   }
 
 
-  private static void loadFileToDictionaryRDF(String file) {
-    if(dictionary == null){
-      dictionary = new RdfDictionaryImport(file);
-    } else{
-      dictionary.putAll(file);
+  private static void loadFileToHashMap(String file) {
+    try (FileHandlerInterface handler = new RdfFileHandler(file)) {
+      if (dictionary == null) {
+        dictionary = new HashMapDictionary(handler);
+      } else {
+        dictionary.putAll(handler);
+      }
+    } catch (IOException e) {
+      log.error(e.getLocalizedMessage(), e);
     }
   }
 
 
-  private static void saveDictionary(){
-    PrintWriter writer = null;
-    try{
-      writer = new PrintWriter(target);
-      for(Entry<String, Set<String>> entry: dictionary.entrySet()){
+  private static void saveDictionary(String file) {
+    try( PrintWriter writer = new PrintWriter(file)) {
+      for (Entry<String, Set<String>> entry : dictionary.entrySet()) {
         writer.print(entry.getKey());
-        for(String value : entry.getValue()){
+        for (String value : entry.getValue()) {
           writer.print("\t" + value);
         }
         writer.println();
       }
-    } catch (Exception e){
-    } finally {
-      writer.close();
+    } catch (IOException e) {
+      log.error(e.getLocalizedMessage(), e);
     }
   }
 }
