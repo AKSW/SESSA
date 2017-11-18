405d155031067c22349d09c38627648a61d95302
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-18 15:45:01 +0100

    Delete unneeded classes
    With the new handlers, the separate dictionary-classes for each file-type are no longer necessary.
    
    Tests are commented out for now, as they probably can be reused to write tests for the handler-classes.

--- src/test/java/org/aksw/sessa/importing/dictionary/implementation/ReverseTsvDictionaryImportTest.java
+++ src/test/java/org/aksw/sessa/importing/dictionary/implementation/ReverseTsvDictionaryImportTest.java
@@ -1,36 +1,36 @@
-package org.aksw.sessa.importing.dictionary.implementation;
-
-import java.util.Set;
-import org.aksw.sessa.importing.dictionary.DictionaryImportInterface;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-public class ReverseTsvDictionaryImportTest {
-
-  private DictionaryImportInterface rtd;
-  private final String fileName = "src/test/resources/small_reverse_dictionary.tsv";
-  @Before
-  public void setUp(){
-    rtd = new ReverseTsvDictionaryImport(fileName);
-  }
-
-  @Test
-  public void get_TestLastElement(){
-    String uri = "http://dbpedia.org/resource/Andriy_Hitchenko";
-    Assert.assertTrue(rtd.get("hitchenko").contains(uri));
-  }
-
-  @Test
-  public void get_TestFirstElement(){
-    String uri = "http://dbpedia.org/resource/List_of_food_companies";
-    Assert.assertTrue(rtd.get("list of food companies").contains(uri));
-
-  }
-
-  @Test
-  public void get_TestSomeMiddleElement(){
-    String uri = "http://dbpedia.org/resource/Rutland_Elementary_School";
-    Assert.assertTrue(rtd.get("rutland elementary school").contains(uri));
-  }
-}
+//package org.aksw.sessa.importing.dictionary.implementation;
+//
+//import java.util.Set;
+//import org.aksw.sessa.importing.dictionary.DictionaryImportInterface;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class ReverseTsvDictionaryImportTest {
+//
+//  private DictionaryImportInterface rtd;
+//  private final String fileName = "src/test/resources/small_reverse_dictionary.tsv";
+//  @Before
+//  public void setUp(){
+//    rtd = new ReverseTsvDictionaryImport(fileName);
+//  }
+//
+//  @Test
+//  public void get_TestLastElement(){
+//    String uri = "http://dbpedia.org/resource/Andriy_Hitchenko";
+//    Assert.assertTrue(rtd.get("hitchenko").contains(uri));
+//  }
+//
+//  @Test
+//  public void get_TestFirstElement(){
+//    String uri = "http://dbpedia.org/resource/List_of_food_companies";
+//    Assert.assertTrue(rtd.get("list of food companies").contains(uri));
+//
+//  }
+//
+//  @Test
+//  public void get_TestSomeMiddleElement(){
+//    String uri = "http://dbpedia.org/resource/Rutland_Elementary_School";
+//    Assert.assertTrue(rtd.get("rutland elementary school").contains(uri));
+//  }
+//}
