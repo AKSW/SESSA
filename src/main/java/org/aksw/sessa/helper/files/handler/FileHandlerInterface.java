daa93fdbdb757534e69d8e4a50af6a620b432a88
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-24 16:40:44 +0100

    Add javadoc to FileHandlerInterface

--- src/main/java/org/aksw/sessa/helper/files/handler/FileHandlerInterface.java
+++ src/main/java/org/aksw/sessa/helper/files/handler/FileHandlerInterface.java
@@ -1,30 +1,35 @@
 package org.aksw.sessa.helper.files.handler;
 
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.Map.Entry;
 
 /**
- * Created by Simon Bordewisch on 17.11.17.
+ * Provides an interface for file handling. The handlers can handle their associate file type and
+ * provide the method nextEntry() to easily stream the file.
+ *
+ * @author Simon Bordewisch
  */
 public interface FileHandlerInterface extends AutoCloseable {
 
   /**
    * Provides next entry, i.e. next key and value pair.
-   * @return next key and value pair.
+   *
+   * @return next key and value pair
    * @throws IOException If an I/O error occurs
    */
   Entry<String, String> nextEntry() throws IOException;
 
   /**
    * Returns name of the file that is processed by the handler.
-   * @return  name of the file that is processed by the handler
+   *
+   * @return name of the file that is processed by the handler
    */
   String getFileName();
 
   /**
    * Closes the used readers and releases any system resources associated with it.
+   *
    * @throws IOException If an I/O error occurs
    */
   @Override
