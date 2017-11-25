5b8a6df9ee61563e0723c7c0814c4cf601feb8d1
Simon Bordewisch <sbordewisch@gmx.de>
2017-11-25 14:40:20 +0100

    Fix field name for term-search to be variable instead of name of variable

--- src/main/java/org/aksw/sessa/importing/dictionary/implementation/LuceneDictionary.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/implementation/LuceneDictionary.java
@@ -140,7 +140,7 @@ public class LuceneDictionary extends FileBasedDictionary implements AutoCloseab
       String[] uniGrams = nGram.split(" ");
       SpanQuery[] queryTerms = new SpanQuery[uniGrams.length];
       for (int i = 0; i < uniGrams.length; i++) {
-        FuzzyQuery fq = new FuzzyQuery(new Term("FIELD_NAME_KEY", uniGrams[i]));
+        FuzzyQuery fq = new FuzzyQuery(new Term(FIELD_NAME_KEY, uniGrams[i]));
         queryTerms[i] = new SpanMultiTermQueryWrapper<>(fq);
       }
       SpanNearQuery wholeQuery = new SpanNearQuery(queryTerms, 0, true);
