4c71cc5e5b72bba71a1b2e4e3aedc6278b76c666
Simon Bordewisch <sbordewisch@gmx.de>
2018-01-07 14:07:32 +0100

    Rework Javadoc in similarity to include idf

--- src/main/java/org/aksw/sessa/importing/dictionary/util/DictionaryEntrySimilarity.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/util/DictionaryEntrySimilarity.java
@@ -3,11 +3,12 @@ package org.aksw.sessa.importing.dictionary.util;
 import org.apache.lucene.search.similarities.DefaultSimilarity;
 
 /**
- * This class overrides the tf-value (term-frequency) to accommodate for the fact that we do not
- * have a document store, but a simple dictionary of n-grams to URIs. Therefore the tf-value is
- * changed to a constant (1).
+ * This class overrides the tf- (term-frequency) and idf-value (inverse document frequency)  to
+ * accommodate for the fact that we do not have a document store, but a simple dictionary of n-grams
+ * to URIs in which we do not need scores based on those values. Therefore the tf-value is changed
+ * to a constant (1).
  *
- * This class is largely based on {@link https://lucene.apache.org/core/3_5_0/api/core/org/apache/lucene/search/package-summary.html#changingSimilarity
+ * This implementation is largely based on {@link https://lucene.apache.org/core/3_5_0/api/core/org/apache/lucene/search/package-summary.html#changingSimilarity
  * this entry}, where it is mentioned to make the above mentioned changed (see second use case).
  *
  * @author Simon Bordewisch
