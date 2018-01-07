8adf4b48f2fe95055339349cecb7e649be74e429
Simon Bordewisch <sbordewisch@gmx.de>
2018-01-07 13:57:14 +0100

    Add Javadoc for AbstractFilter

--- src/main/java/org/aksw/sessa/importing/dictionary/filter/AbstractFilter.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/filter/AbstractFilter.java
@@ -8,19 +8,41 @@ import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Set;
 
+/**
+ * This abstract class is the foundation for making filters for the dictionary classes. Filters
+ * apply after find the URIs and filter them down using a rank. How many results are left is based
+ * on the given parameter.
+ */
 public abstract class AbstractFilter {
 
+  /**
+   * Number of returned results.
+   */
   protected int numberOfResults;
 
+  /**
+   * Constructs the filter which filters the given URIs down to the given number.
+   *
+   * @param numberOfResults specifies the number of returned results
+   */
   public AbstractFilter(int numberOfResults) {
     this.numberOfResults = numberOfResults;
   }
 
-  public Set<Entry<String, String>> filter(String keyword,
-      Set<Entry<String, String>> toBeFiltered) {
+  /**
+   * Main filter method. Returns a number of results with the highest rank (based on the rank
+   * implementation).
+   *
+   * @param keyword keyword with which the entries where found
+   * @param toBeFiltered entry set of n-grams and uris
+   */
+  public Set<Entry<String, String>> filter(
+      String keyword, // e.g. "mark twains" (notice the 's')
+      Set<Entry<String, String>> toBeFiltered //e.g. for entry: <"mark twain", dbr:Mark_Twain">
+  ) {
 
     PriorityQueue<Entry<Entry<String, String>, Float>> sortedResults =
-        new PriorityQueue<>(1000,
+        new PriorityQueue<>(100,
             Collections.reverseOrder(Comparator.comparing(Entry::getValue)));
     for (Entry<String, String> entry : toBeFiltered) {
       float rank = getRank(keyword, entry);
@@ -35,9 +57,21 @@ public abstract class AbstractFilter {
     return finalResultSet;
   }
 
+  /**
+   * Ranking method. Returns a rank/score for given entry and keyword. keyword is given to ensure
+   * that the n-gram in the entry can be compared to the initial keyword.
+   *
+   * @param keyword keyword with which the entries where found
+   * @param entry entry of one n-gram and uri
+   */
   protected abstract float getRank(String keyword, Entry<String, String> entry);
 
-  public int getNumberOfResults(){
+  /**
+   * Returns how many results should be returned
+   *
+   * @return number of returned results
+   */
+  public int getNumberOfResults() {
     return numberOfResults;
   }
 
