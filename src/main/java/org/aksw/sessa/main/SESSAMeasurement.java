772463bfd766c02b691c20f1bcba87a2697dd5c6
Simon Bordewisch <sbordewisch@gmx.de>
2018-01-18 15:46:48 +0100

    Add energy function to SESSAMeasurement

--- src/main/java/org/aksw/sessa/main/SESSAMeasurement.java
+++ src/main/java/org/aksw/sessa/main/SESSAMeasurement.java
@@ -12,6 +12,7 @@ import org.aksw.qa.commons.load.Dataset;
 import org.aksw.qa.commons.load.LoaderController;
 import org.aksw.qa.commons.measure.AnswerBasedEvaluation;
 import org.aksw.sessa.helper.files.handler.RdfFileHandler;
+import org.aksw.sessa.importing.dictionary.energy.EnergyFunctionInterface;
 import org.aksw.sessa.importing.dictionary.energy.LevenshteinDistanceFunction;
 import org.aksw.sessa.importing.dictionary.util.Filter;
 import org.aksw.sessa.importing.dictionary.implementation.LuceneDictionary;
@@ -52,16 +53,19 @@ public class SESSAMeasurement {
     } catch (IOException e) {
       log.error(e.getLocalizedMessage(), e);
     }
-    addFilters();
+    addFiltersAndEnergyFunction();
   }
 
   /**
-   * Use this method for adding filters to SESSA
+   * Use this method for adding filters and manipulate the energy score.
    */
-  private void addFilters() {
+  private void addFiltersAndEnergyFunction() {
     Filter lFilter = new Filter(new LevenshteinDistanceFunction(), 5);
     //Filter pFilter = new Filter(new PagerRankFunction(), 3);
-    //sessa.addFilter(lFilter);
+
+    EnergyFunctionInterface lFunction = new LevenshteinDistanceFunction();
+    sessa.addFilter(lFilter);
+    sessa.setEnergyFunction(lFunction);
     //sessa.addFilter(pRFilter);
 
   }
