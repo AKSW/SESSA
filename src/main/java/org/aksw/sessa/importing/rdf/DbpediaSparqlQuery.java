4747b79a05fdaae06793635824b8cb5943f54457
Simon Bordewisch <sbordewisch@gmx.de>
2018-03-16 16:51:17 +0100

    Add ASK-query method

--- src/main/java/org/aksw/sessa/importing/rdf/DbpediaSparqlQuery.java
+++ src/main/java/org/aksw/sessa/importing/rdf/DbpediaSparqlQuery.java
@@ -1,6 +1,6 @@
 package org.aksw.sessa.importing.rdf;
 
-
+import java.util.Formatter;
 import java.util.HashSet;
 import java.util.Set;
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
@@ -55,6 +55,50 @@ public class DbpediaSparqlQuery {
     }
     log.trace("Query: '{}'. Found: {}", queryString, finalSet);
     return finalSet;
+  }
+
+  /**
+   * Queries an ASK-query to DBpedia with given valid SPARQL ask-query
+   *
+   * @param queryString valid SPARQL ask-query
+   * @return true if ASK-query true, false otherwise
+   */
+  public boolean askQuery(String queryString) {
+
+    QueryExecutionFactory qef = new QueryExecutionFactoryHttp(DBPEDIA_URI, "http://dbpedia.org");
+    qef = new QueryExecutionFactoryRetry(qef, 5, 5000);
+
+    boolean answer = false;
+    Set<String> finalSet = new HashSet<>();
+
+    try {
+      // Add pagination
+      qef = new QueryExecutionFactoryPaginated(qef, 900);
+      QueryExecution qe = qef.createQueryExecution(queryString);
+      answer = qe.execAsk();
+
+    } catch (Exception e) {
+      log.error("Error with query {}", queryString);
+      log.error(e.getLocalizedMessage(), e);
+    }
+    log.trace("Query: '{}'. Found: {}", queryString, finalSet);
+    return answer;
+  }
+
+  /**
+   * Queries an ASK-query to DBpedia with given triple.
+   *
+   * @param subject subject of the triple
+   * @param predicate predicate of the triple
+   * @param object object of the triple
+   * @return true if ASK-query true, false otherwise
+   */
+  public boolean askQuery(String subject, String predicate, String object) {
+    final String QUERY_STRING =
+        "ASK{ <%1$s> <%2$s> <%3$s>. }";
+    Formatter formatter = new Formatter().format(QUERY_STRING, subject, predicate, object);
+    log.debug(formatter.toString());
+    return askQuery(formatter.toString());
   }
 
 }
