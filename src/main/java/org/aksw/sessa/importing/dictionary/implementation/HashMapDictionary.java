9d33d3668c2c1dc7fe6ef4abc229688d7116a15a
Simon Bordewisch <sbordewisch@gmx.de>
2018-03-25 12:55:34 +0200

    Streamline logger

--- src/main/java/org/aksw/sessa/importing/dictionary/implementation/HashMapDictionary.java
+++ src/main/java/org/aksw/sessa/importing/dictionary/implementation/HashMapDictionary.java
@@ -21,7 +21,7 @@ import org.slf4j.LoggerFactory;
  */
 public class HashMapDictionary extends FileBasedDictionary {
 
-  private static final Logger log = LoggerFactory.getLogger(FileBasedDictionary.class);
+  private static final Logger log = LoggerFactory.getLogger(HashMapDictionary.class);
   private Map<String, Set<String>> dictionary;
   /**
    * Initializes the dictionary with given file handler. The file will be parsed into the
