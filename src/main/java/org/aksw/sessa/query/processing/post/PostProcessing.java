a6ce5bf662d55277f540dbdafda2556b9212ec9d
Simon Bordewisch <sbordewisch@gmx.de>
2018-03-25 13:46:21 +0200

    Add handling low information answers

--- src/main/java/org/aksw/sessa/query/processing/post/PostProcessing.java
+++ src/main/java/org/aksw/sessa/query/processing/post/PostProcessing.java
@@ -13,18 +13,35 @@ public class PostProcessing {
 
   private static final Logger log = LoggerFactory.getLogger(PostProcessing.class);
 
-  private final String RDF_TYPE = "type";
   private final String RDF_TYPE_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 
   private final String MESSAGE_FOUND = "Found post processing case";
+  private final int MAX_RESULT_SIZE = 20;
 
 
   public QAModel process(QAModel qAModel) {
     log.debug("Starting post processing...");
     QAModel newQAModel = new QAModel(qAModel);
     Set<Node> results = qAModel.getResults();
+
+    //handling low information answers (too many results)
+    if(results.size() > MAX_RESULT_SIZE) {
+      log.debug("Too many answers(>{}). Returning empty set.", MAX_RESULT_SIZE);
+      newQAModel.setResults(new HashSet<>());
+    }
+    //handling low information answers (explanation score too low)
+    if(qAModel.getExplanationScore() < qAModel.getMaxPossibleExplanationScore()) {
+      log.debug("Explanation score too low (is:{}, maxPossible:{}. Returning empty set.",
+          qAModel.getExplanationScore(),
+          qAModel.getMaxPossibleExplanationScore());
+      newQAModel.setResults(new HashSet<>());
+    }
+
+    // handling specific answer
     if (results.size() == 1) {
       Node node = results.iterator().next();
+
+      // handling rdf:type-answers
       if (node.getContent().toString().equals(RDF_TYPE_URI)) {
         log.debug("{}: rdf:type is the only answer.", MESSAGE_FOUND);
         newQAModel = handleRdfTypeAnswer(qAModel);
