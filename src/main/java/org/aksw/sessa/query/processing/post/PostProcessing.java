da5e052ec3207c03c98cea09854dc6cf2289d1e2
Simon Bordewisch <sbordewisch@gmx.de>
2018-04-05 18:54:11 +0200

    Rework rdf:type-answer processing
    Previously we assumed that the answer is always an instance of a class.
    However, it can be expected that questions like "type of Lion King" can occure (especially if a pre-processor parses questions like "What is Lion King=").
    In this case we want the class instead of the instace.
    This implementation is a rework considers this fact.

--- src/main/java/org/aksw/sessa/query/processing/post/PostProcessing.java
+++ src/main/java/org/aksw/sessa/query/processing/post/PostProcessing.java
@@ -25,12 +25,12 @@ public class PostProcessing {
     Set<Node> results = qAModel.getResults();
 
     //handling low information answers (too many results)
-    if(results.size() > MAX_RESULT_SIZE) {
+    if (results.size() > MAX_RESULT_SIZE) {
       log.debug("Too many answers(>{}). Returning empty set.", MAX_RESULT_SIZE);
       newQAModel.setResults(new HashSet<>());
     }
     //handling low information answers (explanation score too low)
-    if(qAModel.getExplanationScore() < qAModel.getMaxPossibleExplanationScore()) {
+    if (qAModel.getExplanationScore() < qAModel.getMaxPossibleExplanationScore()) {
       log.debug("Explanation score too low (is:{}, maxPossible:{}. Returning empty set.",
           qAModel.getExplanationScore(),
           qAModel.getMaxPossibleExplanationScore());
@@ -59,16 +59,53 @@ public class PostProcessing {
     GraphInterface path = originalGraph.findPathsToNode(rdfType);
     postProcessModel.setGraph(path);
     Set<Node> results = new HashSet<>();
-    log.debug("Searching for nodes that are instance of another");
+
+    // Collect potential result nodes
+    Set<Node> potentialResultSet = new HashSet<>();
     for (Node factNode : path.getNeighborsLeadingTo(rdfType)) {
-      for (Node neighbor1 : path.getNeighborsLeadingTo(factNode)) {
-        for (Node neighbor2 : path.getNeighborsLeadingTo(factNode)) {
-          if (neighbor1 != neighbor2 && isRdfTypeOf(neighbor1, neighbor2)) {
-            log.debug("Found node that is instance of another: {}", neighbor2);
-            results.add(neighbor2);
+      for (Node neighbor : path.getNeighborsLeadingTo(factNode)) {
+        potentialResultSet.add(neighbor);
+      }
+    }
+
+    // Collect initial nodes from potential result nodes
+    Set<Node> initialNodeFromResults = new HashSet<>();
+    for (Node node : potentialResultSet) {
+      if (node.isInitialNode()) {
+        initialNodeFromResults.add(node);
+      }
+    }
+    if (!initialNodeFromResults.isEmpty()) {
+      /* We found initial nodes. This probably means that we can find results which will connect
+       * to all initial nodes.
+       */
+      log.debug(
+          "Found initial nodes. Chances are high that we want to find URIs that are linked with all initial nodes (candidates) via rdf:type.");
+
+      // Removing initial nodes, as they are probably not candidates for the result
+      potentialResultSet.removeAll(initialNodeFromResults);
+
+      for (Node potentialResultNode : potentialResultSet) {
+        boolean isConnectedToAllInitialNodes = true;
+        for (Node initialNode : initialNodeFromResults) {
+          if (!isRdfTypeOf(potentialResultNode, initialNode) &&
+              !isRdfTypeOf(initialNode, potentialResultNode)) {
+            isConnectedToAllInitialNodes = false;
           }
         }
+        if (isConnectedToAllInitialNodes) {
+          results.add(potentialResultNode);
+        }
       }
+      if (results.isEmpty()) {
+        log.debug(
+            "Could not find any potential node that is linked with all initial nodes. Set of initial nodes: {}",
+            initialNodeFromResults);
+      }
+    } else {
+      log.debug(
+          "Could not find initial nodes. Structure seems too complicated. returning all nodes");
+      results.addAll(potentialResultSet);
     }
     postProcessModel.setResults(results);
     return postProcessModel;
