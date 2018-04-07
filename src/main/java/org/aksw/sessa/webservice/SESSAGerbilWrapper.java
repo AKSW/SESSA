ee258eb6a6a789d96b6d86a5f74be9ffb3b8b353
Simon Bordewisch <sbordewisch@gmx.de>
2018-04-07 15:52:33 +0200

    Add loading file via configuration to wrapper

--- src/main/java/org/aksw/sessa/webservice/SESSAGerbilWrapper.java
+++ src/main/java/org/aksw/sessa/webservice/SESSAGerbilWrapper.java
@@ -1,14 +1,24 @@
 package org.aksw.sessa.webservice;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
+import org.aksw.sessa.helper.files.handler.FileHandlerInterface;
 import org.aksw.sessa.helper.files.handler.RdfFileHandler;
+import org.aksw.sessa.helper.files.handler.ReverseTsvFileHandler;
+import org.aksw.sessa.helper.files.handler.TsvFileHandler;
+import org.aksw.sessa.importing.config.ConfigurationInitializer;
 import org.aksw.sessa.importing.config.exception.MalformedConfigurationException;
 import org.aksw.sessa.importing.dictionary.energy.EnergyFunctionInterface;
 import org.aksw.sessa.importing.dictionary.energy.LevenshteinDistanceFunction;
 import org.aksw.sessa.importing.dictionary.util.Filter;
 import org.aksw.sessa.main.SESSA;
+import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
+import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.dice.qa.AbstractQASystem;
 import org.dice.qa.AnswerContainer;
 import org.dice.qa.AnswerContainer.AnswerType;
@@ -22,6 +32,8 @@ import org.slf4j.LoggerFactory;
 public class SESSAGerbilWrapper extends AbstractQASystem {
 
   private static final Logger log = LoggerFactory.getLogger(SESSAGerbilWrapper.class);
+  private static final String FILES_KEY = "dictionary.files.location";
+
   private SESSA sessa;
   private String RDF_labels = "src/main/resources/dbpedia_3Eng_class.ttl";
   private String RDF_ontology = "src/main/resources/dbpedia_3Eng_property.ttl";
@@ -33,22 +45,54 @@ public class SESSAGerbilWrapper extends AbstractQASystem {
    */
   public SESSAGerbilWrapper() throws MalformedConfigurationException {
     sessa = new SESSA();
+    BaseHierarchicalConfiguration configuration = ConfigurationInitializer.getConfiguration();
     long startTime = System.nanoTime();
-    try {
-      log.info("No Lucene Dictionary found.");
-      log.info("Building Lucene Dictionary from RDF files. This could take some time!");
-      //Change the handler and the file to be handled here
-      sessa.loadFileToDictionary(new RdfFileHandler(RDF_labels));
-      sessa.loadFileToDictionary(new RdfFileHandler(RDF_ontology));
-      sessa.loadFileToDictionary(new RdfFileHandler(RDF_DBpedia_Ontology));
-      sessa.loadFileToDictionary(new RdfFileHandler(RDF_DBpedia_Labels));
-      long endTime = System.nanoTime();
-      log.info("Finished importing Lucene Dictionary (in {}sec).",
-          (endTime - startTime) / (1000 * 1000 * 1000));
-    } catch (IOException e) {
-      log.error(e.getLocalizedMessage());
-    }
+    loadDictionaries(sessa, configuration);
+    long endTime = System.nanoTime();
+    log.info("Finished importing Lucene Dictionary (in {}sec).",
+        (endTime - startTime) / (1000 * 1000 * 1000));
     addFiltersAndEnergyFunction();
+  }
+
+  private void loadDictionaries(SESSA sessa, BaseHierarchicalConfiguration configuration) {
+    HierarchicalConfiguration subConfig = configuration.configurationAt(FILES_KEY);
+    if (subConfig.containsKey("rdf")) {
+      log.info("Found entry for rdf-files in configuration file. Importing...");
+      loadSingleDictionary(sessa, new RdfFileHandler(), subConfig.getString("rdf"));
+    }
+    if (subConfig.containsKey("tsv")) {
+      log.info("Found entry for tsv-files in configuration file. Importing...");
+      loadSingleDictionary(sessa, new TsvFileHandler(), subConfig.getString("tsv"));
+    }
+    if (subConfig.containsKey("reverse_tsv")) {
+      log.info("Found entry for reverse tsv-files in configuration file. Importing...");
+      loadSingleDictionary(sessa, new ReverseTsvFileHandler(), subConfig.getString("reverse_tsv"));
+    }
+  }
+
+
+  private void loadSingleDictionary(SESSA sessa, FileHandlerInterface handler,
+      String pathString) {
+    log.debug("Path to files is '{}'", pathString);
+    try (Stream<Path> path = Files.walk(Paths.get(pathString))) {
+      path
+          .filter(Files::isRegularFile)
+          .forEach((Path file) -> {
+            try {
+              log.info("Loading file '{}' to dictionary via {}.",
+                  file.toString(),
+                  handler.getClass().getSimpleName());
+              handler.loadFile(file.toString());
+              sessa.loadFileToDictionary(handler);
+            } catch (IOException ioE) {
+              log.error(ioE.getLocalizedMessage());
+            }
+          });
+    } catch (IOException ioE) {
+      log.error(ioE.getLocalizedMessage());
+      log.info(
+          "Could not load any file in given path '{}'.", pathString);
+    }
   }
 
   /**
